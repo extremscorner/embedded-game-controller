ed37a5eef4d9368261731846c24fe6292f5d5c37
Alberto Mardegan <mardy@users.sourceforge.net>
2026-03-19 19:32:21 +0300

    wii: implement wait_events
    Also migrate to the tuxedo API.

--- embedded-game-controller/backends/wii.c
+++ embedded-game-controller/backends/wii.c
@@ -1,12 +1,12 @@
 #include <assert.h>
-#include <ogc/lwp.h>
 #include <ogc/machine/processor.h>
-#include <ogc/message.h>
-#include <ogc/mutex.h>
 #include <ogc/system.h>
 #include <ogc/usb.h>
 #include <stdio.h>
 #include <string.h>
+#include <tuxedo/mailbox.h>
+#include <tuxedo/ppc/clock.h>
+#include <tuxedo/tick.h>
 
 #include "platform.h"
 #include "utils.h"
@@ -39,9 +39,7 @@ typedef struct {
         // TODO: add bluetooth data here
     };
     egc_input_state_t next_input;
-    /* This mutex ensures that the input report is always updated in full */
-    mutex_t next_input_mutex;
-    syswd_t timer_id;
+    KTickTask timer_task;
     egc_timer_cb timer_callback;
 } wii_device_t;
 
@@ -63,25 +61,16 @@ typedef enum {
     WII_EVENT_GOT_INPUT,
 } wii_event_e;
 
-typedef struct wii_event_queue_t {
-    struct wii_event_t {
-        wii_event_e type;
-        u8 device_index;
-    } ATTRIBUTE_PACKED events[WII_MAX_EVENTS];
-    u8 current_index;
-} ATTRIBUTE_PACKED wii_event_queue_t;
-static wii_event_queue_t s_event_queue;
-static mutex_t s_event_queue_mutex;
-
 static wii_device_t s_devices[MAX_ACTIVE_DEVICES] ATTRIBUTE_ALIGN(32);
 static wii_transfer_t s_transfers[MAX_ACTIVE_TRANSFERS] ATTRIBUTE_ALIGN(32);
-static mqbox_t s_worker_queue;
-static lwp_t s_worker_thread;
+static KMailbox s_worker_queue;
+static uptr s_worker_queue_slots[10];
 static egc_event_cb s_event_handler;
 
+static void wii_device_free(wii_device_t *device);
 static int update_device_list(void);
-#define MSG_UPDATE_DEVICES_LIST ((mqmsg_t)update_device_list)
-#define MSG_FROM_TRANSFER(t)    ((mqmsg_t)t)
+#define MSG_UPDATE_DEVICES_LIST ((uptr)update_device_list)
+#define MSG_FROM_TRANSFER(t)    ((uptr)t)
 
 static inline wii_device_t *wii_device_from_input_device(egc_input_device_t *input_device)
 {
@@ -137,7 +126,7 @@ static s32 wii_transfer_cb(s32 result, void *userdata)
         transfer->t.status = EGC_USB_TRANSFER_STATUS_ERROR;
         transfer->t.length = 0;
     }
-    MQ_Send(s_worker_queue, MSG_FROM_TRANSFER(transfer), MQ_MSG_NOBLOCK);
+    KMailboxTrySend(&s_worker_queue, MSG_FROM_TRANSFER(transfer));
     return result;
 }
 
@@ -203,56 +192,42 @@ static const egc_usb_transfer_t *wii_intr_transfer_async(egc_input_device_t *inp
 
 /* This is called from an interrupt: do nothing in here, just send the event to
  * the worker thread */
-static void timer_cb(syswd_t timer_id, void *userdata)
+static void timer_cb(KTickTask *timer)
 {
-    wii_device_t *device = userdata;
-    MQ_Send(s_worker_queue, &device->timer_id, MQ_MSG_NOBLOCK);
-}
-
-static void us_to_timespec(int time_us, struct timespec *ts)
-{
-    ts->tv_sec = time_us / 1000000;
-    ts->tv_nsec = (time_us % 1000000) * 1000;
+    KMailboxTrySend(&s_worker_queue, (uptr)timer);
 }
 
 static int wii_set_timer(egc_input_device_t *input_device, int time_us, int repeat_time_us,
                          egc_timer_cb callback)
 {
     wii_device_t *device = (wii_device_t *)input_device;
-    struct timespec time0, time1;
-    int rc;
 
-    if (device->timer_id != 0) {
-        SYS_CancelAlarm(device->timer_id);
-    } else {
-        SYS_CreateAlarm(&device->timer_id);
+    if (device->timer_task.fn != NULL) {
+        KTickTaskStop(&device->timer_task);
     }
 
-    us_to_timespec(time_us, &time0);
-    if (repeat_time_us > 0) {
-        us_to_timespec(repeat_time_us, &time1);
-        rc = SYS_SetPeriodicAlarm(device->timer_id, &time0, &time1, timer_cb, device);
-    } else {
-        rc = SYS_SetAlarm(device->timer_id, &time0, timer_cb, device);
-    }
+    u64 timeout_ticks = PPCUsToTicks(time_us);
+    u64 period_ticks = (repeat_time_us > 0) ? PPCUsToTicks(repeat_time_us) : 0;
+    KTickTaskStart(&device->timer_task, timer_cb, timeout_ticks, period_ticks);
     device->timer_callback = callback;
-    return rc == 0 ? 0 : -1;
+    return 0;
 }
 
-static bool queue_event(egc_event_e type, wii_device_t *device)
+static bool report_event(wii_event_e type, wii_device_t *device)
 {
-    if (s_event_queue.current_index >= WII_MAX_EVENTS) {
-        LOG_INFO("WII event queue is full!");
-        return false;
+    if (type == WII_EVENT_DEVICE_ADDED) {
+        int ret = s_event_handler(&device->pub, EGC_EVENT_DEVICE_ADDED, device->usb.dev.vid,
+                                  device->usb.dev.pid);
+        if (ret != 0) {
+            wii_device_free(device);
+        }
+    } else if (type == WII_EVENT_DEVICE_REMOVED) {
+        s_event_handler(&device->pub, EGC_EVENT_DEVICE_REMOVED);
+        /* Mark this device as not valid */
+        wii_device_free(device);
+    } else if (type == WII_EVENT_GOT_INPUT) {
+        memcpy(&device->pub.state, &device->next_input, sizeof(device->next_input));
     }
-
-    u8 device_index = device - s_devices;
-
-    LWP_MutexLock(s_event_queue_mutex);
-    struct wii_event_t *e = &s_event_queue.events[s_event_queue.current_index++];
-    e->type = type;
-    e->device_index = device_index;
-    LWP_MutexUnlock(s_event_queue_mutex);
     return true;
 }
 
@@ -260,11 +235,6 @@ static void wii_device_free(wii_device_t *device)
 {
     if (device->usb.fd >= 0) {
         USB_CloseDevice(&device->usb.fd);
-    }
-
-    if (device->next_input_mutex != 0) {
-        LWP_MutexDestroy(device->next_input_mutex);
-        device->next_input_mutex = 0;
     }
 
     device->pub.connection = EGC_CONNECTION_DISCONNECTED;
@@ -281,7 +251,7 @@ static void wii_device_free(wii_device_t *device)
  * the worker thread */
 int change_notify_cb(int result, void *userdata)
 {
-    MQ_Send(s_worker_queue, MSG_UPDATE_DEVICES_LIST, MQ_MSG_NOBLOCK);
+    KMailboxTrySend(&s_worker_queue, MSG_UPDATE_DEVICES_LIST);
     return result;
 }
 
@@ -318,7 +288,7 @@ static int update_device_list(void)
                      " got disconnected\n",
                      device->usb.dev.vid, device->usb.dev.pid, device->usb.dev.device_id);
 
-            if (!queue_event(EGC_EVENT_DEVICE_REMOVED, device)) {
+            if (!report_event(WII_EVENT_DEVICE_REMOVED, device)) {
                 /* We normally mark the device as not valid only after the
                  * client has retrieved the event, but since we couldn't add
                  * the event into the queue, let's mark the device as available
@@ -360,12 +330,11 @@ static int update_device_list(void)
         }
 
         /* We have ownership, populate the device info */
-        LWP_MutexInit(&device->next_input_mutex, false);
-        device->timer_id = 0;
+        memset(&device->timer_task, 0, sizeof(device->timer_task));
         device->timer_callback = NULL;
         device->pub.connection = EGC_CONNECTION_USB;
 
-        queue_event(EGC_EVENT_DEVICE_ADDED, device);
+        report_event(WII_EVENT_DEVICE_ADDED, device);
     }
 
     /* Continue watching for device changes */
@@ -374,25 +343,42 @@ static int update_device_list(void)
     return 0;
 }
 
-static void *worker_thread(void *unused)
+static void process_events_timeout(KTickTask *)
+{
+    KMailboxTrySend(&s_worker_queue, (uptr)process_events_timeout);
+}
+
+static int process_events(u32 timeout_us)
 {
     bool done = false;
+    int count = 0;
 
-    update_device_list();
+    KTickTask timer;
+    if (timeout_us != 0) {
+        u64 timeout_ticks = PPCUsToTicks(timeout_us);
+        KTickTaskStart(&timer, process_events_timeout, timeout_ticks, 0);
+    }
 
     while (!done) {
-        mqmsg_t message;
+        uptr message;
 
-        bool ok = MQ_Receive(s_worker_queue, &message, MQ_MSG_BLOCK);
-        if (!ok) {
-            /* Try again? */
-            continue;
+        if (!KMailboxTryRecv(&s_worker_queue, &message)) {
+            if (timeout_us != 0) {
+                message = KMailboxRecv(&s_worker_queue);
+                done = true;
+            } else {
+                break;
+            }
         }
 
         if (message == MSG_UPDATE_DEVICES_LIST) {
+            count++;
             update_device_list();
+        } else if (message == (uptr)process_events_timeout) {
+            done = true;
         } else if (message >= MSG_FROM_TRANSFER(&s_transfers[0]) &&
                    message <= MSG_FROM_TRANSFER(&s_transfers[ARRAY_SIZE(s_transfers) - 1])) {
+            count++;
             /* It's a transfer reply */
             for (int i = 0; i < ARRAY_SIZE(s_transfers); i++) {
                 wii_transfer_t *transfer = &s_transfers[i];
@@ -414,18 +400,22 @@ static void *worker_thread(void *unused)
                 wii_device_t *device = &s_devices[i];
                 if (device->pub.connection == EGC_CONNECTION_DISCONNECTED)
                     continue;
-                if (message == &device->timer_id) {
+                if (message == (uptr)&device->timer_task) {
+                    count++;
                     LOG_INFO("%s:%d timer on %p\n", __func__, __LINE__, device);
                     bool keep = device->timer_callback(&device->pub);
                     if (!keep) {
-                        SYS_RemoveAlarm(device->timer_id);
-                        device->timer_id = 0;
+                        KTickTaskStop(&device->timer_task);
                     }
                 }
             }
         }
     }
-    return NULL;
+
+    if (timeout_us != 0) {
+        KTickTaskStop(&timer);
+    }
+    return count;
 }
 
 static int wii_init(egc_event_cb event_handler)
@@ -438,21 +428,12 @@ static int wii_init(egc_event_cb event_handler)
     if (rc != USB_OK)
         return -1;
 
-    rc = MQ_Init(&s_worker_queue, 10);
-    if (rc != MQ_ERROR_SUCCESSFUL)
-        return -1;
-
-    rc = LWP_MutexInit(&s_event_queue_mutex, false);
-    if (rc != 0)
-        return -1;
+    KMailboxPrepare(&s_worker_queue, s_worker_queue_slots, ARRAY_SIZE(s_worker_queue_slots));
 
     for (int i = 0; i < ARRAY_SIZE(s_devices); i++)
         s_devices[i].pub.connection = EGC_CONNECTION_DISCONNECTED;
 
-    /* Give our thread a little more priority than the default */
-    u8 priority = (LWP_PRIO_IDLE + LWP_PRIO_HIGHEST) / 4;
-    rc = LWP_CreateThread(&s_worker_thread, worker_thread, NULL, NULL, 0, priority);
-    return rc == 0 ? 0 : -1;
+    return update_device_list();
 }
 
 static egc_device_description_t *wii_alloc_desc(egc_input_device_t *device)
@@ -488,44 +469,14 @@ static int wii_set_suspended(egc_input_device_t *input_device, bool suspended)
 static int wii_report_input(egc_input_device_t *input_device, const egc_input_state_t *state)
 {
     wii_device_t *device = (wii_device_t *)input_device;
-    LWP_MutexLock(device->next_input_mutex);
     memcpy(&device->next_input, state, sizeof(*state));
-    LWP_MutexUnlock(device->next_input_mutex);
-    queue_event(WII_EVENT_GOT_INPUT, device);
+    report_event(WII_EVENT_GOT_INPUT, device);
     return 0;
 }
 
-static int wii_handle_events()
+static int wii_wait_events(u32 timeout_us)
 {
-    wii_event_queue_t queue;
-    /* Create a copy of the queue to ensure it doesn't get modified while we
-     * process it */
-    LWP_MutexLock(s_event_queue_mutex);
-    memcpy(&queue, &s_event_queue, sizeof(queue));
-    s_event_queue.current_index = 0; /* empty the queue */
-    LWP_MutexUnlock(s_event_queue_mutex);
-
-    for (int i = 0; i < queue.current_index; i++) {
-        struct wii_event_t *event = &queue.events[i];
-        wii_device_t *device = &s_devices[event->device_index];
-
-        if (event->type == WII_EVENT_DEVICE_ADDED) {
-            int ret = s_event_handler(&device->pub, EGC_EVENT_DEVICE_ADDED, device->usb.dev.vid,
-                                      device->usb.dev.pid);
-            if (ret != 0) {
-                wii_device_free(device);
-            }
-        } else if (event->type == WII_EVENT_DEVICE_REMOVED) {
-            s_event_handler(&device->pub, EGC_EVENT_DEVICE_REMOVED);
-            /* Mark this device as not valid */
-            wii_device_free(device);
-        } else if (event->type == WII_EVENT_GOT_INPUT) {
-            LWP_MutexLock(device->next_input_mutex);
-            memcpy(&device->pub.state, &device->next_input, sizeof(device->next_input));
-            LWP_MutexUnlock(device->next_input_mutex);
-        }
-    }
-    return queue.current_index;
+    return process_events(timeout_us);
 }
 
 const egc_platform_backend_t _egc_platform_backend = {
@@ -539,5 +490,5 @@ const egc_platform_backend_t _egc_platform_backend = {
     .alloc_desc = wii_alloc_desc,
     .set_timer = wii_set_timer,
     .report_input = wii_report_input,
-    .handle_events = wii_handle_events,
+    .wait_events = wii_wait_events,
 };
